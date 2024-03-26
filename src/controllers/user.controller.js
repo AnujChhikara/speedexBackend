7383d3ff6c731b8cb2200517797f6de3ebde11c7
AnujChhikara <anujchhikara777@gmail.com>
2024-03-26 20:26:29 +0530

    add login route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import { User } from "../models/user.model.js"
+import bcrypt from 'bcrypt'
 
 const registerUser = asyncHandler( async (req, res) => {
    const {email, fullName, password, gender} = req.body
@@ -30,7 +31,7 @@ const registerUser = asyncHandler( async (req, res) => {
    //sending userData into frontend without password
 
     const createdUser = await User.findById(user._id).select(
-        "-password -refreshToken"
+        "-password"
     )
    
 
@@ -38,4 +39,40 @@ const registerUser = asyncHandler( async (req, res) => {
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req,res) =>{
+    const {email, password} = req.body
+
+    //checking if user entered both fileds
+
+    if(!email || !password ){
+        throw new ApiError(402, "All filed required")
+       }
+
+       //cheking if user exist in database
+
+       const user = await User.findOne({email})
+
+       if(!user) {
+        throw new ApiError(402, "User does not exist")
+       }
+
+       //cheking if password matched
+
+       const isPasswordMatched = await bcrypt.compare(password, user.password)
+
+       if(!isPasswordMatched){
+        throw new ApiError(404, "User Credentials are wrong")
+       }
+
+       //sending user data wihtout password
+
+       const userData = await User.findById(user._id).select("-password")
+
+
+
+       return res.status(200).json(new ApiResponse(200, userData,"User logged in successfully" ))
+
+})
+
+export {registerUser,
+loginUser}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
 router.route("/register").post(registerUser)
+router.route("/login").post(loginUser)
 
-router.route("/home").get((req,res) => {
-    res.send("<h1>Hello</h1>")
-})
+
 
 export default router
